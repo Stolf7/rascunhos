aab75641bd6948d89b2d699b6ec40886a6189171
Matheus Stolf <116499516+Stolf7@users.noreply.github.com>
2023-04-07 11:49:08 -0300

    Update Gerenciarfuncionarios.java

--- Gerenciarfuncionarios.java
+++ Gerenciarfuncionarios.java
@@ -11,14 +11,16 @@ public class GerenciarFuncionarios {
         GerenciarFuncionarios gf = new GerenciarFuncionarios();
         int opcao = 0;
         do {
-            System.out.println("\_____________________________/");
-            System.out.println("Gerenciador de Funcionarios..");
-            System.out.println("\_____________________________/");
+            System.out.println("_______________________________");
+            System.out.println("..GERENCIADOR DE FUNCIONARIOS..");
+            System.out.println("_______________________________");
             System.out.println("1 - Cadastrar Funcionario");
-            System.out.println("2 - Consultar Funcionario");
-            System.out.println("3 - Bonificar Unico Funcionario");
+            System.out.println("2 - Consultar lista de Funcionarios");
+            System.out.println("3 - Consultar Unico Funcionario");
             System.out.println("4 - Bonificar todos os funcionarios");
+            System.out.println("5 - Bonificar Unico Funcionario");
             System.out.println("9 - Sair");
+            System.out.println("_______________________________");
             System.out.println("O que voce deseja: ");
 
             opcao = Integer.parseInt(gf.sc.nextLine());
@@ -31,10 +33,16 @@ public class GerenciarFuncionarios {
                     gf.execConsultar();
                     break;
                 case 3:
-                    gf.execBonificarUnico();
+                    gf.execConsultarUnico();
                     break;
                 case 4:
-                    gf.execConsultarUnico();
+                    gf.execBonificar();
+                    break;
+                case 5:
+                    gf.execBonificarUnico();
+                    break;
+                case 9:
+                    System.out.println("Saindo do programa...\nFinalizado com Sucesso.");
                     break;
                 default:
                     System.out.println("Opcao Invalida");
@@ -42,6 +50,7 @@ public class GerenciarFuncionarios {
             }
 
         } while (opcao!=9);
+
     }
 
     public void execCadastrar(){
@@ -50,33 +59,65 @@ public class GerenciarFuncionarios {
         System.out.println("Registro: ");
         long registro = Long.parseLong(sc.nextLine());
         System.out.println("Esta Ativo [SIM/NAO]: ");
-        boolean estaAtivo = Boolean.parseBoolean(sc.nextLine());
+        String estaAtivo = Boolean.parseBoolean(sc.nextLine());
+        if (estaAtivo.equals("SIM") || estaAtivo.equals("sim")) {
+            estaAtivo = true;
+        } else {
+            estaAtivo = false;
+        }
         System.out.println("Nome: ");
         String nome = sc.nextLine();
         System.out.println("Departamento: ");
         String departamento = sc.nextLine();
         System.out.println("RG: ");
         String rg = sc.nextLine();
-        
-        Funcionarios funcionarios = new Funcionario(salario, registro, estaAtivo, nome, departamento, rg);
-        listaFuncionarios.add(funcionarios);
-    }
 
-    public void execConsultarUnico(){
-
+        Funcionarios funcionarios1 = new Funcionarios(salario, registro, estaAtivo, nome, departamento, rg);
+        listaFuncionarios.add(funcionarios1);
     }
 
     public void execConsultar(){
-
+        System.out.println("Consulta de todos os funcionarios..");
+        for (Funcionarios funcionarios: listaFuncionarios) {
+            funcionarios.imprimirTodos();
+            System.out.println("Fim da Lista");
+        }
     }
 
-    public void execBonificarUnico(){
-
+    public void execConsultarUnico(){
+        System.out.println("Informe o registro do funcionario: ");
+        long registro = Long.parseLong(sc.nextLine());
+        for (Funcionarios funcionarios1: listaFuncionarios) {
+            if (funcionarios1.getRegistro() == registro) {
+                System.out.println("Funcionario encontrado com sucesso");
+                funcionarios1.imprimir();
+            } else {
+                System.out.println("Funcionario não encontrado");
+            }
+        }
     }
 
     public void execBonificar(){
-
+        System.out.println("informe o valor da bonificação: ");
+        double valor = Double.parseDouble(sc.nextLine());
+        for (Funcionarios funcionarios1: listaFuncionarios) {
+            funcionarios1.setSalario(funcionarios1.getSalario() + valor);
+        }
+        System.out.println("Funcionarios Bonificados!");
     }
 
+    public void execBonificarUnico(){
+        System.out.println("Informe o Registro do funcionario que voce deseja bonificar: ");
+        long registro = Long.parseLong(sc.nextLine());
+        for (Funcionarios funcionarios1: listaFuncionarios) {
+            if (funcionarios1.getRegistro() == registro) {
+                System.out.println("Informe o valor da bonificacao para esse funcionario");
+                double valor = Double.parseDouble(sc.nextLine());
+                funcionarios1.setSalario(funcionarios1.getSalario() + valor);
+            } else {
+                System.out.println("Funcionario não encontrado");
+            }
+        }
+    }
 
 }
